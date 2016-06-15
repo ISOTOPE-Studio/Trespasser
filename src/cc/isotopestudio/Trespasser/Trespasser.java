2aa304e7b0016127ede88fdf61d279af6633d9c8
Mars Tan <davidtjx424@live.com>
2016-06-15 20:26:01 +0800

    Change the method to detect player's location

--- src/cc/isotopestudio/Trespasser/Trespasser.java
+++ src/cc/isotopestudio/Trespasser/Trespasser.java
@@ -1,10 +1,8 @@
 package cc.isotopestudio.Trespasser;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -12,16 +10,25 @@ import org.bukkit.scheduler.BukkitRunnable;
  * Created by Mars on 6/14/2016.
  * Copyright ISOTOPE Studio
  */
-public class Trespasser extends JavaPlugin implements Listener {
+public class Trespasser extends JavaPlugin {
 
     private static final String pluginName = "Trespasser";
-    static final String prefix = (new StringBuilder()).append(ChatColor.GOLD).append(ChatColor.BOLD).append("[")
+    private static final String prefix = (new StringBuilder()).append(ChatColor.GOLD).append(ChatColor.BOLD).append("[")
             .append("系统").append("]").append(ChatColor.RED).toString();
 
     @Override
     public void onEnable() {
-
-        getServer().getPluginManager().registerEvents(this, this);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (final Player player : Bukkit.getOnlinePlayers()) {
+                    if (!player.hasPermission("world." + player.getLocation().getWorld().getName())) {
+                        player.setHealth(0);
+                        player.sendMessage(prefix + "你没有权限在这个世界");
+                    }
+                }
+            }
+        }.runTaskTimer(this, 40, 20);
         getLogger().info(pluginName + "成功加载!");
         getLogger().info(pluginName + "由ISOTOPE Studio制作!");
         getLogger().info("http://isotopestudio.cc");
@@ -32,17 +39,4 @@ public class Trespasser extends JavaPlugin implements Listener {
         getLogger().info(pluginName + "成功卸载!");
     }
 
-    @EventHandler
-    public void onTp(PlayerTeleportEvent event) {
-        final Player player = event.getPlayer();
-        if (!player.hasPermission("world" + event.getTo().getWorld().getName())) {
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    player.setHealth(0);
-                    player.sendMessage(prefix + "你没有权限在这个世界");
-                }
-            }.runTaskLater(this, 2);
-        }
-    }
 }
